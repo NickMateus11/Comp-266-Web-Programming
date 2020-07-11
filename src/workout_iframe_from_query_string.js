4d56bfdcc78ff875cd1e553d62c629ae83d8daf8
Nicholas Mateus <nicholas.mateus@ibm.com>
2020-07-11 16:08:41 -0400

    Update to search query code

--- src/workout_iframe_from_query_string.js
+++ src/workout_iframe_from_query_string.js
@@ -1,18 +1,10 @@
 
-function getParameterByName(name, url) {
-    if (!url) {
-        url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, '\\$&');
-    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-    const results = regex.exec(url);
-    if (!results) {
-        return null;
-    }
-    if (!results[2]) {
-        return '';
-    }
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getParameterByName(name) {
+    const regex = new RegExp(name + '=(\\w+)'); // Will extract the value from ex: 'name=Value'
+    const results = regex.exec(window.location.search);
+    if (!results) return null; // Regex failed to match anything
+    if (!results[1]) return ''; // The name of the parameter did not exist
+    return decodeURIComponent(results[1].replace(/\+/g, ' ')); // Replace '+' with ' '
 }
 
 const workout = getParameterByName("workout");
